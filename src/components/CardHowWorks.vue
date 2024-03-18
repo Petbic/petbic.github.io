36bc88e32a3d2d76a26f2f3c23d3bb079426f428
Patri <patri@malvidopazo.es>
2024-03-18 20:37:06 +0100

    feat: create component card in vue

---
+++ src/components/CardHowWorks.vue
@@ -0,0 +1,35 @@
+<template>
+  <template v-if="showText">
+    <div class="container flex flex-col items-center gap-4 md:gap-6">
+      <img :src="image" :alt="alt" />
+      <p class="text-lg text-white lg:text-xl">
+        {{ title }}
+      </p>
+      <button @click="showText = !showText">
+        <img src="../../public/img/down-arrow.png" alt="arrow" />
+      </button>
+    </div>
+  </template>
+  <template v-else>
+    <div class="container flex flex-col items-center gap-4 md:gap-6">
+      <button @click="showText = !showText">
+        <img src="../../public/img/up-arrow.png" alt="arrow" />
+      </button>
+      <p class="mb-6 text-white text-center font-bold">{{ description }}</p>
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps({
+  title: { type: String },
+  image: { type: String },
+  alt:{type:String},
+  description:{type:String}
+})
+
+const showText = ref(true)
+
+</script>
