b96733113bd9a5077df19fd6697f8d8663e3b7f5
David Fernández Calle <david.fernandez@nextail.co>
2024-06-12 17:33:15 +0200

    PETBIC-41 - Add github action to deploy in gihub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://petib.github.io',
   integrations: [
     tailwind({
       // Example: Permite escribir declaraciones CSS anidadas
